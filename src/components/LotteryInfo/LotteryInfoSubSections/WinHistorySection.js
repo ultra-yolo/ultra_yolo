fe7fb3c320a523623d7f2ec670a78b05081283d8
ultra-yolo <ultrayolooo@gmail.com>
2018-01-09 14:04:19 +0700

    add favicon and yarn eject

--- src/components/LotteryInfo/LotteryInfoSubSections/WinHistorySection.js
+++ src/components/LotteryInfo/LotteryInfoSubSections/WinHistorySection.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { SubSectionContainer } from "./LotteryInfoSubSections.style";
 
 class WinHistorySection extends Component {
